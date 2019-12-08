9085446c446c332b93854a5baa9d53c8d1f9bdd2
Tomas Tobar <codetombomb@gmail.com>
2019-12-08 20:51:44 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -43,6 +43,7 @@ def total_gross(source)
   # 2. list_of_directors: names provides an Array of directors names (use
 # use list_of_directors to find the key in director totals.
 list_of_directors(source)[0]
+binding.pry
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
