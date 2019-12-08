e59575cbfe5d34516406c76ee4a6e31979d2fa95
Tomas Tobar <codetombomb@gmail.com>
2019-12-08 20:30:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,7 +29,7 @@ def list_of_directors(source)
   list = []
   director_list = 0
   while director_list < source.size do
-    list << directors_database[director_list][:name]
+    list << source[director_list][:name]
     director_list += 1
     # binding.pry
   end
